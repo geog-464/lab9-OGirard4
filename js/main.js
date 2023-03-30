cb6bcf2d361623754bb413ee790fcd9435d60dc9
OGirard4 <olivier.girard@comcast.net>
2023-03-30 15:22:28 -0400

    upload lab 9

--- js/main.js
+++ js/main.js
@@ -3,14 +3,20 @@ let myMap;
 
 // we might as well declare our baselayer(s) here too
 const CartoDB_Positron = L.tileLayer(
-	'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', 
+	'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', 
 	{
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+		attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}
 )
+const OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', 
+{
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+});
+
 
 //add the basemap style(s) to a JS object, to which you could also add other baselayers. This object is loaded as a basemap selector as seen further down
-let baseLayers = {
+let baseLayers = { 
 	"CartoDB": CartoDB_Positron
 	//,...
 };
@@ -19,20 +25,72 @@ function initialize(){
     loadMap();
 };
 
-function loadMap(){
+function loadMap(mapid){
 	//now reassign the map variable by actually making it a useful object, this will load your leaflet map
-	myMap = L.map('mapdiv', {
-		center: [45.50, -73.58]
-		,zoom: 3
-		,maxZoom: 18
-		,minZoom: 3
-		,layers: CartoDB_Positron
+	try {
+	myMap.remove()
+} 	catch(e) {
+	console.log(e)
+	console.log("no map to delete")
+} 	finally {
+	//put your map loading code in here
+		if (mapid == 'mapa'){
+		myMap = L.map('mapdiv', {
+			center: [45.50, -73.58]
+			,zoom: 3
+			,maxZoom: 18
+			,minZoom: 3
+			,layers: OpenStreetMap_HOT
 	});
-
-	//declare basemap selector widget
-	let lcontrol = L.control.layers(baseLayers);
-	//add the widget to the map
-	lcontrol.addTo(myMap);
+		fetchData("https://raw.githubusercontent.com/geog-464/geog-464.github.io/main/Amtrak_Stations.geojson")
+		} else if (mapid == 'mapb'){
+		myMap = L.map('mapdiv', {
+			center: [45.50, -73.58]
+			,zoom: 3
+			,maxZoom: 18
+			,minZoom: 3
+			,layers: CartoDB_Positron
+	})
+		fetchData("https://raw.githubusercontent.com/geog-464/geog-464.github.io/main/megacities.geojson")	
+	}}
 };
+function fetchData(e){
+    //load the data
+    fetch(e)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer using the fetched json and add it to the map object
+            L.geoJson(json,{style: styleAll, pointToLayer: generateCircles, onEachFeature: addPopups}).addTo(myMap)
+        })
+};
+function addPopups(feature, layer){
+	layer.bindPopup(feature.properties.StationNam);
+}
+function generateCircles(feature, latlng) {
+	return L.circleMarker(latlng);
+}
 
-window.onload = initialize();
+function styleAll(feature, latlng) {
+	console.log(feature.properties.ZipCode)
+	var styles = {dashArray:null, dashOffset:null, lineJoin:null, lineCap:null, stroke:false, color:'#000', opacity:1, weight:1, fillColor:null, fillOpacity:0 };
+
+	if (feature.geometry.type == "Point") {
+		styles.fillColor = '#fff'
+		,styles.fillOpacity = 0.5
+		,styles.stroke=true
+		,styles.radius=9
+	}
+	if (typeof feature.properties.ZipCode == "string") {
+		styles.fillColor = 'cyan'
+		,styles.fillOpacity = 0.5
+		,styles.stroke=true
+		,styles.radius=9
+
+	}
+
+
+	return styles;
+}
+//window.onload = initialize();
